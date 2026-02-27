172dda11428cd7edf8c29f429c0614b407724233
harinibaskar <hbaskar@calpoly.edu>
2026-02-26 20:02:37 -0800

    finalchanges

--- outsourcer/src/main/java/csc364/Main.java
+++ outsourcer/src/main/java/csc364/Main.java
@@ -8,6 +8,8 @@ public class Main {
         int numLocalWorkers = 2;
         int numRemoteWorkers = 2;
 
+        String broker = "tcp://test.mosquitto.org:1883";  
+
         String[] jobs = {
             "2 + 3",
             "17 - 1",
@@ -21,7 +23,9 @@ public class Main {
             try {
                 for (String job : jobs) repository.produce(job);
                 // Add "STOP" signals for local workers
-                for (int i = 0; i < numLocalWorkers; i++) repository.produce("STOP");
+                for (int i = 0; i < numLocalWorkers; i++) {
+                    repository.produce("STOP");
+                }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
@@ -31,34 +35,40 @@ public class Main {
         // --- Step 2: Start LocalWorkers ---
         Thread[] localWorkers = new Thread[numLocalWorkers];
         for (int i = 0; i < numLocalWorkers; i++) {
-            localWorkers[i] = new Thread(new LocalWorker(repository, i + 1), "LocalWorker-" + (i + 1));
+            localWorkers[i] = new Thread(
+                new LocalWorker(repository, i + 1),
+                "LocalWorker-" + (i + 1)
+            );
             localWorkers[i].start();
         }
 
         // --- Step 3: Start Outsourcer ---
-        Outsourcer outsourcer = new Outsourcer("tcp://localhost:1883", "Outsourcer");
+        Outsourcer outsourcer = new Outsourcer(broker, "Outsourcer");  
 
         // Push jobs from Repository to Outsourcer queue for RemoteWorkers
         while (!repository.queueIsEmpty()) {
             String job = repository.consume();
-            if (!job.equals("STOP")) outsourcer.addJob(job);
+            if (!job.equals("STOP")) {
+                outsourcer.addJob(job);
+            }
         }
 
         // --- Step 4: Start RemoteWorkers ---
         Thread[] remoteWorkers = new Thread[numRemoteWorkers];
         for (int i = 0; i < numRemoteWorkers; i++) {
-            RemoteWorker rw = new RemoteWorker("tcp://localhost:1883", "Worker" + (i + 1));
+            RemoteWorker rw = new RemoteWorker(broker, "Worker" + (i + 1));  
             remoteWorkers[i] = new Thread(rw, "RemoteWorker-" + (i + 1));
             remoteWorkers[i].start();
         }
 
         // Wait for local workers to finish
-        for (Thread w : localWorkers) w.join();
+        for (Thread w : localWorkers) {
+            w.join();
+        }
 
         // Wait for producer to finish
         producer.join();
 
-        // RemoteWorkers run indefinitely, can add logic to stop after all jobs done
         System.out.println("Local workers done, Outsourcer + RemoteWorkers running...");
     }
 }
